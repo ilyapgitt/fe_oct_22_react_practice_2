d785f87eb7ee0f0f3b896ce64efd120193d179ee
Illia Ptah <ilyaptakh@gmail.com>
2023-11-28 13:01:12 +0200

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import cn from 'classnames';
 
-// import usersFromServer from './api/users';
-// import photosFromServer from './api/photos';
-// import albumsFromServer from './api/albums';
+import usersFromServer from './api/users';
+import photosFromServer from './api/photos';
+import albumsFromServer from './api/albums';
 
-export const App: React.FC = () => {
+const photos = photosFromServer.map((photo) => {
+  const albums = albumsFromServer.find(album => album.id === photo.albumId);
+
+  const user = usersFromServer.find(client => client.id === albums.userId);
+
+  return {
+    ...photo,
+    albums,
+    user,
+  };
+});
+
+export const App = () => {
+  const FEMALE = 'f';
+
+  const [query, setQuery] = useState('');
+  const visiblePhotos = photos.filter((photo) => {
+    const lowerQuerty = query.toLowerCase().trim();
+    const { title } = photo;
+
+    return title.toLowerCase().includes(lowerQuerty);
+  });
+
+  // const [selectedTab, setSelectedTab] = useState(photos[0].id);
+  // const onTabSelected = {tab => setSelectedTab(tab.id)}
+  // const activeTabId = visiblePhotos.some(tab => tab.id === selectedTab)
+  //   ? selectedTab
+  //   : photos.user[0].id;
+
+  // const choozeTabClick = (tab) => {
+  //   if (activeTabId !== tab.id) {
+  //     onTabSelected(tab)
+  //   }
+  // };
+
   return (
     <div className="section">
       <div className="container">
@@ -22,24 +57,14 @@ export const App: React.FC = () => {
                 All
               </a>
 
-              <a
-                href="#/"
-              >
-                User 1
-              </a>
-
-              <a
-                href="#/"
-                className="is-active"
-              >
-                User 2
-              </a>
-
-              <a
-                href="#/"
-              >
-                User 3
-              </a>
+              {usersFromServer.map(user => (
+                <a
+                  data-cy="FilterUser"
+                  href="#/"
+                >
+                  {user.name}
+                </a>
+              ))}
             </p>
 
             <div className="panel-block">
@@ -48,63 +73,52 @@ export const App: React.FC = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
-                  value="qwe"
+                  value={query}
+                  onChange={event => setQuery(event.target.value)}
                 />
 
                 <span className="icon is-left">
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
 
-                <span className="icon is-right">
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    type="button"
-                    className="delete"
-                  />
-                </span>
+                {query && (
+                  <span className="icon is-right">
+                    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                    <button
+                      type="button"
+                      className="delete"
+                      onClick={() => setQuery('')}
+                    />
+                  </span>
+                )}
               </p>
             </div>
 
             <div className="panel-block is-flex-wrap-wrap">
               <a
                 href="#/"
-                className="button is-success mr-6 is-outlined"
+                className="button is-success mr-6 is-outlined is-info"
               >
                 All
               </a>
 
-              <a
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Album 1
-              </a>
-
-              <a
-                className="button mr-2 my-1"
-                href="#/"
-              >
-                Album 2
-              </a>
-
-              <a
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Album 3
-              </a>
-              <a
-                className="button mr-2 my-1"
-                href="#/"
-              >
-                Album 4
-              </a>
-              <a
-                className="button mr-2 my-1"
-                href="#/"
-              >
-                Album 5
-              </a>
+              {albumsFromServer.map(album => (
+                <a
+                  href="#/"
+                  className="button mr-2 my-1"
+                  style={{
+                    maxWidth: '200px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                  }}
+                >
+                  {
+                    album.title.length > 25
+                      ? `${album.title.substring(0, 25)}...`
+                      : album.title
+                  }
+                </a>
+              ))}
             </div>
 
             <div className="panel-block">
@@ -180,18 +194,23 @@ export const App: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="has-text-weight-bold">
-                  1
-                </td>
+              {visiblePhotos.map(photo => (
+                <tr key={photo.id}>
+                  <td className="has-text-weight-bold">
+                    {photo.id}
+                  </td>
 
-                <td>accusamus beatae ad facilis cum similique qui sunt</td>
-                <td>quidem molestiae enim</td>
+                  <td>{photo.title}</td>
+                  <td>{photo.albums.title}</td>
 
-                <td className="has-text-link">
-                  Max
-                </td>
-              </tr>
+                  <td className={cn(photo.user.sex === FEMALE
+                    ? 'has-text-danger'
+                    : 'has-text-link')}
+                  >
+                    {photo.user.name}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
